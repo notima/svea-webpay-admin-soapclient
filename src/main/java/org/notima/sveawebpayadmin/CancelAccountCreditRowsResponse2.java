a3234a7de1143c885cfead1f6395e9bb1d4bb819
Daniel Tamm <daniel@notima.se>
2018-11-19 14:36:40 +0100

    Regenerate code from WSDL on 2018-11-19.
    Refactor package from se.sveaekonomi.partnerweb to
    org.notima.sveawebpayadmin

---
+++ src/main/java/org/notima/sveawebpayadmin/CancelAccountCreditRowsResponse2.java
@@ -0,0 +1,64 @@
+
+package org.notima.sveawebpayadmin;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="CancelAccountCreditRowsResult" type="{http://schemas.datacontract.org/2004/07/DataObjects.Admin.Service.Responses}CancelAccountCreditRowsResponse" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "cancelAccountCreditRowsResult"
+})
+@XmlRootElement(name = "CancelAccountCreditRowsResponse", namespace = "http://tempuri.org/")
+public class CancelAccountCreditRowsResponse2 {
+
+    @XmlElement(name = "CancelAccountCreditRowsResult", namespace = "http://tempuri.org/", nillable = true)
+    protected CancelAccountCreditRowsResponse cancelAccountCreditRowsResult;
+
+    /**
+     * Gets the value of the cancelAccountCreditRowsResult property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CancelAccountCreditRowsResponse }
+     *     
+     */
+    public CancelAccountCreditRowsResponse getCancelAccountCreditRowsResult() {
+        return cancelAccountCreditRowsResult;
+    }
+
+    /**
+     * Sets the value of the cancelAccountCreditRowsResult property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CancelAccountCreditRowsResponse }
+     *     
+     */
+    public void setCancelAccountCreditRowsResult(CancelAccountCreditRowsResponse value) {
+        this.cancelAccountCreditRowsResult = value;
+    }
+
+}
