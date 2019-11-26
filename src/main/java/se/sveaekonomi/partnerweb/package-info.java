0f4765f73e9b6c2bd5bbb8f17aff940cf797e807
Daniel Tamm <daniel@notima.se>
2019-11-26 14:59:58 +0100

    Refactor package and name of API.

--- src/main/java/se/sveaekonomi/partnerweb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/DataObjects.Admin.Service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.sveaekonomi.partnerweb;
